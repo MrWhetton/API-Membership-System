31fa6d198343b40cbf34bbac07bebd1ca7cd0722
MrWhetton <jackwhetton@s-d-s.co.uk>
2022-08-09 16:56:40 +0100

    no more return

--- public/index.js
+++ public/index.js
@@ -2,47 +2,45 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
   get: (searchParams, prop) => searchParams.get(prop),
 });
 
-const {cardID} = params
-const quit = () =>  document.location="/DemoCardList.html"
+const { cardID } = params
+const quit = () => document.location = "/DemoCardList.html"
 const textvalInput = document.querySelector("#textval")
 const heading = document.querySelector("h1")
 
-let currentCard = {} 
+let currentCard = {}
 let currentEmployee = {}
 
 
-if (!cardID){
+if (!cardID)
   quit()
-  return 
-}
-
-fetch (`/Card?CardID=${cardID}`)
-.catch(error => {
-  //Takes you back to the DemoCardList Screen
-  quit()
-})
-.then(result => result.json())
-.then(card => {
-    currentCard = card
-    const {employeeID} = card
-    return fetch(`/Employee?employeeID=${employeeID}`)
-})
-.then(result => result.json())
-.then(employee => {
-  currentEmployee = employee
-  heading.innerText=`Welcome ${employee.name} please input your PIN!`
-})
+else
+  fetch(`/Card?CardID=${cardID}`)
+    .catch(error => {
+      //Takes you back to the DemoCardList Screen
+      quit()
+    })
+    .then(result => result.json())
+    .then(card => {
+      currentCard = card
+      const { employeeID } = card
+      return fetch(`/Employee?employeeID=${employeeID}`)
+    })
+    .then(result => result.json())
+    .then(employee => {
+      currentEmployee = employee
+      heading.innerText = `Welcome ${employee.name} please input your PIN!`
+    })
 
 
-const display = (digit) => textvalInput.value += digit 
-const clear = () => textvalInput.value=""
+const display = (digit) => textvalInput.value += digit
+const clear = () => textvalInput.value = ""
 
-const confirm = () => {
+const enter = () => {
   const pin = textvalInput.value
   const correctPIN = (currentCard.PIN == pin)
   if (!correctPIN)
     return clear()
-  
+
   // pin is correct
   window.localStorage['currentEmployee'] = JSON.stringify(currentEmployee)
   document.location = 'TopUp.html'
