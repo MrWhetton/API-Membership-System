5f8f7b3a57e9c93bc995720807757f82c736c13a
MrWhetton <jackwhetton@s-d-s.co.uk>
2022-08-09 13:27:20 +0100

    remove a dot

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('./public/index.html');
+const router = jsonServer.router('/public/index.html');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
