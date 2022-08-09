d1915e3fdd5d8af77c04ca9f70821817ea6cbd5b
MrWhetton <jackwhetton@s-d-s.co.uk>
2022-08-09 14:01:35 +0100

    trying to the index.html to display on the browser

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults({
-  static = 'public', 
+  static = 'public/index.html', 
   noCors = true
 });
 const port = process.env.PORT || 3000;
