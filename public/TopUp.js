52a3821fd296e3d631bcd669d14707a90d17d736
MrWhetton <jackwhetton@s-d-s.co.uk>
2022-08-10 11:28:03 +0100

    added missing const heading and fixed indentation

--- public/TopUp.js
+++ public/TopUp.js
@@ -2,12 +2,13 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
   get: (searchParams, prop) => searchParams.get(prop),
 });
 
+const heading = document.querySelector("h1")
 let currentEmployee = {}
 
 fetch(`/Employee?employeeID=${employeeID}`)
-.then(result => result.json())
-.then(employee => {
-  currentEmployee = employee[0]
-  heading.innerText = `${currentEmployee.name} please input amount you need?`
-  console.log(currentEmployee.name)
-})
+  .then(result => result.json())
+  .then(employee => {
+    currentEmployee = employee[0]
+    heading.innerText = `${currentEmployee.name} please input amount you need?`
+    console.log(currentEmployee.name)
+  })
