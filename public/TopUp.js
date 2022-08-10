d9dc6c3285401defa4798d7322508ab9aeb3f771
MrWhetton <jackwhetton@s-d-s.co.uk>
2022-08-10 14:21:06 +0100

    Get & Post request implementation

--- public/TopUp.js
+++ public/TopUp.js
@@ -1,3 +1,4 @@
+debugger
 const params = new Proxy(new URLSearchParams(window.location.search), {
   get: (searchParams, prop) => searchParams.get(prop),
 });
@@ -5,19 +6,23 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
 const { cardID } = params
 const quit = () => document.location = "/DemoCardList.html"
 const heading = document.querySelector("h1")
-const inputAmountValue = document.querySelector("#textval")
+const inputAmountValue = document.querySelector("#money")
 
 let currentEmployee = {}
+let currentBalance = {}
 
-
+// trying to fetch money from db.json // get request ?
   fetch(`/Employee?employeeID=${employeeID}`)
+    .then( money => {
+        currentBalance = money
+        return fetch(`/Employee?money=${money}`)
+      })
     .then(result => result.json())
-    .then(employee => {
-      currentEmployee = employee[0]
-      heading.innerText = `${currentEmployee.name} please input amount you need?`
-      console.log(currentEmployee.name)
+    // trying to copy what i did on index.js
+    .then(Totalbalance => {
+      Totalbalance = currentBalance
+      heading.innerText = `current balance on card is ${Totalbalance} `
     })
-
 
 const display = (digit) => inputAmountValue.value += digit
 const clear = () => inputAmountValue.value = ""
@@ -29,4 +34,15 @@ const enter = () => {
   return 
     alert('input amount is over the personally limit')
     clear()
+
+  // post request // the internet sucks on provide clear understanding of
+  // post request without thrid party performing the it for you!!!!
+
+  let updateOfBalance = await fetch('/employee/money',{
+    method: 'POST',
+    headers: {
+      'Content-Type' : 'application/json'
+    },
+    body: JSON.stringify(money)
+  })
 }
