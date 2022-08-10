af30ff2e08e3335a7e0f5f0408eabfa83b55e059
MrWhetton <jackwhetton@s-d-s.co.uk>
2022-08-10 12:04:35 +0100

    implementing Get request code

--- public/TopUp.js
+++ public/TopUp.js
@@ -2,13 +2,31 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
   get: (searchParams, prop) => searchParams.get(prop),
 });
 
+const { cardID } = params
+const quit = () => document.location = "/DemoCardList.html"
 const heading = document.querySelector("h1")
+const inputAmountValue = document.querySelector("#textval")
+
 let currentEmployee = {}
 
-fetch(`/Employee?employeeID=${employeeID}`)
-  .then(result => result.json())
-  .then(employee => {
-    currentEmployee = employee[0]
-    heading.innerText = `${currentEmployee.name} please input amount you need?`
-    console.log(currentEmployee.name)
-  })
+
+  fetch(`/Employee?employeeID=${employeeID}`)
+    .then(result => result.json())
+    .then(employee => {
+      currentEmployee = employee[0]
+      heading.innerText = `${currentEmployee.name} please input amount you need?`
+      console.log(currentEmployee.name)
+    })
+
+
+const display = (digit) => inputAmountValue.value += digit
+const clear = () => inputAmountValue.value = ""
+
+const enter = () => {
+  const money = inputAmountValue.value
+  const MaxAmount = 50
+  if (money > MaxAmount)
+  return 
+    alert('input amount is over the personally limit')
+    clear()
+}
