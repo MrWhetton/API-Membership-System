5d7744bec39876c15fdcdd793931b0fb08d71364
MrWhetton <jackwhetton@s-d-s.co.uk>
2022-08-11 17:05:20 +0100

    uppper case for Employee

--- public/TopUp.js
+++ public/TopUp.js
@@ -32,7 +32,7 @@ const enter = () => {
     return
   }
   currentEmployee.money = currentBalance 
-  fetch('/employee/' + currentEmployee.employeeID, { 
+  fetch('/Employee/' + currentEmployee.employeeID, { 
     method: 'PATCH',
     headers: {'Content-Type' : 'application/json'},
     body: JSON.stringify({money: currentBalance})
