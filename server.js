80d323a101e9b5a0a1610716fd2e72f9d5b54521
MrWhetton <jackwhetton@s-d-s.co.uk>
2022-08-11 16:23:46 +0100

    test

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const bodyParser = require('body-parser');
 const express = require('express');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults({
+const server = express.create();
+const router = express.router('db.json');
+const middlewares = express.defaults({
   static : 'public', 
   noCors : true
 });
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
-server.use(jsonServer.bodyParser)
+server.use(express.bodyParser)
 server.use(router);
 server.listen(port);
