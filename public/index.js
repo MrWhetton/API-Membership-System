4ab1dd5e1515024add0e0fad168dec4bd5f0de0b
MrWhetton <jackwhetton@s-d-s.co.uk>
2022-08-12 10:56:48 +0100

    deploying debugger index.js

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-// debugger - to help test in source code on inspect on the hmtl
+debugger
 const params = new Proxy(new URLSearchParams(window.location.search), {
   get: (searchParams, prop) => searchParams.get(prop),
 });
@@ -38,8 +38,8 @@ else
       heading.innerText = `Welcome ${currentEmployee.name} please input your PIN!`
       console.log(currentEmployee.name)
     })
-
-setTimeout(quit, 100 * 2 * 60)
+.
+setTimeout(quit, 1000 * 2 * 60)
 
 const display = (digit) => {
   textvalInput.value += digit
